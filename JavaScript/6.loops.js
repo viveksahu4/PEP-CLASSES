93ebdfb42d98ceba2729dcbfe1114779c1591dc1
Vivek sahu <vickysv1610@gmail.com>
2026-02-08 18:40:43 +0530

    Loops

---
+++ JavaScript/6.loops.js
@@ -0,0 +1,60 @@
+//for loop
+//nested for loop 
+//while loop 
+//do while loop 
+
+
+
+
+
+
+
+
+// for loop 
+
+// for(i = 0 ; i <=85;i++){
+//     console.log(i);
+
+// }
+
+// for(i = 1 ; i<=100; i+=2){
+//     console.log(i);
+// }
+
+
+
+// // while loop 
+// let i =  15;
+// while(i<=0){
+//     console.log(i);
+//     i--;
+// }
+
+
+
+// do {
+// console.log("hello bro!!!")
+// i++;
+// }
+// while(i >= 18);
+
+
+
+
+
+
+
+// // odd number b/w 50 to 80\
+// for(i=51;i<=80;i+=2){
+//     console.log(i);
+// }
+
+
+// nested loop table print 
+
+for(let i=1;i<=3;i++){
+    console.log("Table of "+ i);
+    for(let j=1;j<=3;j++){
+        console.log(i + "x" + j + " = " + i * j);
+    }
+}
