6080f9632abbd8522ea95cf6c0553c3e7ec06005
Vivek sahu <vickysv1610@gmail.com>
2026-02-08 14:57:47 +0530

    Conditional

---
+++ JavaScript/5.conditional.js
@@ -0,0 +1,43 @@
+//if
+//if else 
+//if elseif else 
+
+
+
+// let a = prompt("Enter the CGPA: ");
+// if(a >=6.5){
+//     console.log("you can eligible for placement");
+
+// }
+// else {
+//     console.log("You can't eligible for placement");
+// }
+
+// let color = prompt("Enter signal traffic color that you seen: ");
+
+// if( color == "yello" || color == "Yello"){
+//     console.log("Go Slowely");
+// }
+// else if ( color == "green" || color == "Green"){
+//     console.log("You can Go");
+// }
+// else if ( color == "red" || color == "Red"){
+//     console.log("Alert Stop ");
+// }
+// else {
+//     console.log("Enter valid  signal traffic color choose like yellow red green only");
+// }
+
+
+
+let color = prompt("Enter signal color: ");
+if(color == "Red"){
+    console.log("You must stop");
+}
+else if(color == "Green"){
+    console.log("You can go");
+}
+else{
+    console.log("Slow down");
+}
+
